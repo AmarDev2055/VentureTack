fd61249517666d351b88562dd621b56565ae8183
Amar Dev <devamar029@gmail.com>
2023-04-01 20:28:13 +0545

    Final changes and full submit

--- src/components/Layout/Report.js
+++ src/components/Layout/Report.js
@@ -54,7 +54,100 @@ const Report = () => {
           />
         </form>
       </div>
-      <div className="report">Report</div>
+      <div className="report">
+        <div className="list">
+          <button
+            class="btn btn-light mb-2 dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            १०
+          </button>
+          ओटा मात्र देखाऊनुस
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">क्र.स.</th>
+              <th scope="col">स्थानीय तहको नाम</th>
+              <th scope="col">जिल्ला</th>
+              <th scope="col">स्थानीय तहको प्रकारहरू</th>
+              <th scope="col">प्रदेश</th>
+              <th scope="col">वेबसाइट</th>
+              <th scope="col">कैफियत</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+            <tr>
+              <th scope="row">6</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+            <tr>
+              <th scope="row">7</th>
+              <td>आठराई आठराई गाउँपालिका</td>
+              <td>ताप्लेजुङ</td>
+              <td>गाउँपालिका</td>
+              <td>प्रदेश नम्बर १</td>
+              <td>Delivered</td>
+              <td>तयार</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
